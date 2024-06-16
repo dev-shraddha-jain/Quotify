ca9e3121863293a48fe01c7f1d2940e65b9b45f6
Shraddha <shraddhatongya97@gmail.com>
2024-06-16 16:23:20 +0530

    ui refinement

--- app/src/main/java/com/groot/quotify/ui/module/home/HomeScreen.kt
+++ app/src/main/java/com/groot/quotify/ui/module/home/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.groot.quotify.ui.module.home
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,12 +21,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.groot.quotify.R
 import com.groot.quotify.base.QuoteApiModel
 import com.groot.quotify.dto.QotdResponse
 import com.groot.quotify.ui.module.shared.ButtonFlatView
-import com.groot.quotify.ui.module.shared.CustomTopAppBar
+import com.groot.quotify.ui.theme.Blue0
+import com.groot.quotify.ui.theme.QuotifyTheme
+import com.groot.quotify.ui.theme.gradient
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -42,28 +54,54 @@ fun HomeScreen(navController: NavHostController) {
 
     Scaffold(
         topBar = {
-            CustomTopAppBar("Hello!!")
         },
         content = {
             Column(
                 modifier = Modifier
-                    .padding(it),
+                    .fillMaxSize()
+                    .padding(it)
+                    .background(gradient),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Row(
+                Text(
                     modifier = Modifier
-                        .padding(top = 25.dp)
-                        .padding(horizontal = 25.dp)
-                        .border(1.dp, Color.Gray, MaterialTheme.shapes.small)
+                        .fillMaxWidth()
+                        .padding(25.dp),
+                    style = MaterialTheme.typography.titleLarge,
+                    text = stringResource(id = R.string.app_name),
+                    textAlign = TextAlign.Center
+                )
+
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(25.dp)
+                        .wrapContentHeight(),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = CardDefaults.cardColors(
+                        contentColor = Color.Transparent
+                    ),
+                    border = CardDefaults.outlinedCardBorder(
+
+                    ),
+                    elevation = CardDefaults.cardElevation(2.dp)
                 ) {
-                    Text(
+                    Row(
                         modifier = Modifier
-                            .padding(25.dp),
-                        text = quoteOfTheDay.value?.quote?.body ?: "Loading..."
-                    )
+                            .padding(12.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(25.dp),
+                            text = quoteOfTheDay.value?.quote?.body ?: "Loading...",
+                            color = Blue0
+                        )
+                    }
                 }
+
 
                 ButtonFlatView("Re-Generate") {
                     GlobalScope.launch {
@@ -73,6 +111,15 @@ fun HomeScreen(navController: NavHostController) {
             }
         }
     )
+}
+
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    QuotifyTheme {
+        HomeScreen(rememberNavController())
+    }
 }
 
 
--- app/src/main/java/com/groot/quotify/ui/theme/Color.kt
+++ app/src/main/java/com/groot/quotify/ui/theme/Color.kt
@@ -1,11 +1,44 @@
 package com.groot.quotify.ui.theme
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.center
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RadialGradientShader
+import androidx.compose.ui.graphics.ShaderBrush
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
+val Blue0 = Color(0xFF4158D0)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val pink = Color(0xFFEEAECA)
+val blue = Color(0xFF94BBE9)
+
+val gradientStart = Color(0xFFe9b7ce)
+val gradientMiddle = Color(0xFFd3f3f1)
+val gradientEnd = Color(0xFFd3f3f1)
+val largeRadialGradient = object : ShaderBrush() {
+    override fun createShader(size: Size): androidx.compose.ui.graphics.Shader {
+        val biggerDimension = maxOf(size.height, size.width)
+        return RadialGradientShader(
+            colors = listOf(blue, pink),
+            center = size.center,
+            radius = biggerDimension / 2f,
+            colorStops = listOf(0f, 0.95f)
+        )
+    }
+}
+
+val gradient = Brush.linearGradient(
+    0.0f to Color(0xFF4158D0),
+    0.46f to Color(0xFFC850C0),
+    1.0f to Color(0xFFFFCC70),
+    start = Offset.Zero,
+    end = Offset.Infinite
+)
