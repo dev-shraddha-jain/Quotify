3a3b742df5ebbbc7c28a471c53748f9aa436332e
Shraddha <shraddhatongya97@gmail.com>
2025-03-22 13:57:07 +0530

    addded code

--- app/src/main/java/com/groot/quotify/ui/module/home/HomeScreen.kt
+++ app/src/main/java/com/groot/quotify/ui/module/home/HomeScreen.kt
@@ -1,47 +1,56 @@
 package com.groot.quotify.ui.module.home
 
-import androidx.compose.foundation.background
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.groot.quotify.R
 import com.groot.quotify.base.QuoteApiModel
 import com.groot.quotify.dto.QotdResponse
-import com.groot.quotify.ui.module.shared.ButtonFlatView
-import com.groot.quotify.ui.theme.Blue0
 import com.groot.quotify.ui.theme.QuotifyTheme
-import com.groot.quotify.ui.theme.gradient
+import com.groot.quotify.ui.theme.platinum
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
 
     val apiViewModel = QuoteApiModel()
 
+    val context = LocalContext.current
     val quoteOfTheDay = remember { mutableStateOf<QotdResponse?>(null) }
 
     LaunchedEffect(key1 = Unit) {
@@ -54,59 +63,129 @@ fun HomeScreen(navController: NavHostController) {
 
     Scaffold(
         topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+
+                },
+                modifier = Modifier.fillMaxWidth(),
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                            .size(24.dp),
+                        content = {
+                            Image(
+                                painter = painterResource(R.drawable.ic_baseline_view_list_24),
+                                contentDescription = "list"
+                            )
+                        }
+                    )
+                },
+                actions = {
+
+                    IconButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .padding(end = 12.dp)
+                            .size(24.dp),
+                        content = {
+                            Image(
+                                painter = painterResource(R.drawable.ic_baseline_theme_24),
+                                contentDescription = "theme"
+                            )
+                        },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            
+                        )
+                    )
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
+            )
         },
         content = {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(it)
-                    .background(gradient),
-                verticalArrangement = Arrangement.Center,
+                    .padding(it),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Text(
+                Row(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(25.dp),
-                    style = MaterialTheme.typography.titleLarge,
-                    text = stringResource(id = R.string.app_name),
-                    textAlign = TextAlign.Center
-                )
-
-
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(25.dp)
-                        .wrapContentHeight(),
-                    shape = RoundedCornerShape(12.dp),
-                    colors = CardDefaults.cardColors(
-                        contentColor = Color.Transparent
-                    ),
-                    border = CardDefaults.outlinedCardBorder(
-
-                    ),
-                    elevation = CardDefaults.cardElevation(2.dp)
+                        .padding(12.dp)
+                        .weight(0.8f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Row(
+                    Text(
                         modifier = Modifier
-                            .padding(12.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .padding(25.dp),
-                            text = quoteOfTheDay.value?.quote?.body ?: "Loading...",
-                            color = Blue0
-                        )
-                    }
+                            .fillMaxWidth()
+                            .padding(25.dp),
+                        text = quoteOfTheDay.value?.quote?.body ?: "Loading...",
+                        textAlign = TextAlign.Center
+                    )
                 }
 
 
-                ButtonFlatView("Re-Generate") {
-                    GlobalScope.launch {
-                        quoteOfTheDay.value = apiViewModel.getQuoteOfTheDay()
-                    }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                        .height(45.dp),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+
+                    Text(
+                        text = "Tap for more",
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                GlobalScope.launch {
+                                    quoteOfTheDay.value = apiViewModel.getQuoteOfTheDay()
+                                }
+                            },
+                        color = platinum
+                    )
+
+                    Spacer(Modifier.weight(1f))
+
+                    IconButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier.size(24.dp),
+                        content = {
+                            Image(
+                                painter = painterResource(R.drawable.ic_baseline_bookmark_border_24),
+                                contentDescription = "save"
+                            )
+                        }
+                    )
+
+
+                    IconButton(
+                        onClick = {
+                            val sendIntent = Intent(Intent.ACTION_SEND)
+                            sendIntent.putExtra(Intent.EXTRA_TEXT, quoteOfTheDay.value?.quote?.body ?: "Loading...")
+                            sendIntent.setType("text/plain")
+                            startActivity(context, sendIntent, null)
+                        },
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                            .size(24.dp),
+                        content = {
+                            Image(
+                                painter = painterResource(R.drawable.ic_baseline_ios_share_24),
+                                contentDescription = "share"
+                            )
+                        }
+                    )
                 }
             }
         }
@@ -114,7 +193,7 @@ fun HomeScreen(navController: NavHostController) {
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
     QuotifyTheme {
--- app/src/main/java/com/groot/quotify/ui/theme/Color.kt
+++ app/src/main/java/com/groot/quotify/ui/theme/Color.kt
@@ -1,12 +1,6 @@
 package com.groot.quotify.ui.theme
 
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.center
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RadialGradientShader
-import androidx.compose.ui.graphics.ShaderBrush
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -23,22 +17,6 @@ val blue = Color(0xFF94BBE9)
 val gradientStart = Color(0xFFe9b7ce)
 val gradientMiddle = Color(0xFFd3f3f1)
 val gradientEnd = Color(0xFFd3f3f1)
-val largeRadialGradient = object : ShaderBrush() {
-    override fun createShader(size: Size): androidx.compose.ui.graphics.Shader {
-        val biggerDimension = maxOf(size.height, size.width)
-        return RadialGradientShader(
-            colors = listOf(blue, pink),
-            center = size.center,
-            radius = biggerDimension / 2f,
-            colorStops = listOf(0f, 0.95f)
-        )
-    }
-}
 
-val gradient = Brush.linearGradient(
-    0.0f to Color(0xFF4158D0),
-    0.46f to Color(0xFFC850C0),
-    1.0f to Color(0xFFFFCC70),
-    start = Offset.Zero,
-    end = Offset.Infinite
-)
+val jetBlack = Color(0xFF343434)
+val platinum = Color(0xFFE5E4E2)
